d359c32bb26ffebb00487ebf859d80c72b936c00
Noya_5 <valenoya7@gmail.com>
2023-08-24 11:52:51 -0300

    small changes

--- src/components/MainCointainter.jsx
+++ src/components/MainCointainter.jsx
@@ -1,8 +1,6 @@
-import { Link } from "react-router-dom"
-
 import classes from './MainContainer.module.css'
 import Display from "./ValueDisplay";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Calculator from "./Calculator";
 
 const MainContainer = () => {
@@ -21,7 +19,7 @@ const MainContainer = () => {
         <button onClick={()=>setValue(0)} className={value === 0 ? classes.button_active : classes.button}>Oficial</button>
         <button onClick={()=>setValue(1)} className={value === 1 ? classes.button_active : classes.button}>Blue</button>
         <button onClick={()=>setValue(2)} className={value === 2 ? classes.button_active : classes.button}>Bolsa</button>
-        <button onClick={()=>setValue(3)} className={value === 3 ? classes.button_active : classes.button}>Contado</button>
+        <button onClick={()=>setValue(3)} className={value === 3 ? classes.button_active : classes.button}>CCL</button>
         <button onClick={()=>setValue(4)} className={value === 4 ? classes.button_active : classes.button}>Solidario</button>
         <button onClick={()=>setValue(5)} className={value === 5 ? classes.button_active : classes.button}>Mayorista</button>
       </nav>
